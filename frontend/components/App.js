f993c3c426cc36478b1469fe3c12049b64dd56e8
Joseph Noblett <noblettjoe@gmail.com>
2023-09-18 01:18:30 -0500

    analyzed the API Data with Postman, got todos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          todos: res.data.data
+        })
+        })
+      .catch(err => console.error(err))
+
+  }
+
+  componentDidMount() {
+    console.log('CDM')
+    //get todos
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    console.log('Render Ran')
+    return (
+      <div className='App'>
+        <div id='error'>Error: No error here</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <div id='todoForm'>
+          <input type='text' placeholder='Type Todo'></input>
+          <input type='submit'></input>
+        </div>
+      </div>
+    )
   }
 }
